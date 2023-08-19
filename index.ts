6e233d22f8432c2ac919b6542717a5d820c1718d
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-19 22:49:16 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import { fromEvent, audit, interval } from 'rxjs';
 
+// https://rxjs.dev/api/operators/audit
+
 const clicks = fromEvent(document, 'click');
-const result = clicks.pipe(audit(ev => interval(1000)));
-result.subscribe(x => console.log(x));
+
+const result = clicks.pipe(audit((ev) => interval(2000)));
+
+result.subscribe((x) => console.log(x));
